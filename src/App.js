27c61865b07c2fb4dd47dd0f7ce0390041db4ec0
bobbyhadz <opmftw@gmail.com>
2024-04-07 15:49:37 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  console.log('hello world');
+  console.log('bobbyhadz.com');
+
+  return (
+    <div>
+      <h2>bobbyhadz.com</h2>
+    </div>
+  );
+}
+
+export default App;
